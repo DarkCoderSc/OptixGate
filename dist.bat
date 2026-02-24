93dd0d5edee0100d1ae5428113ba3f66a71437da
DarkCoderSc <jplesueur@proton.me>
2026-02-24 22:08:22 +0100

    Progress in 2.0.1

--- dist.bat
+++ dist.bat
@@ -1,9 +1,35 @@
 @echo off
+setlocal enabledelayedexpansion
 if exist "Dist" rd /s /q "Dist"
 
 REM Prepare Delphi Env
+set "BDS_REG_KEY=HKLM\SOFTWARE\WOW6432Node\Embarcadero\BDS"
+set "LATEST_VERSION=0.0"
+set "DELPHI_PATH="
+
+for /f "tokens=6 delims=\" %%A in ('reg query "%BDS_REG_KEY%" /f * /k') do (
+    set "CURRENT_VERSION=%%A"
+    if !CURRENT_VERSION! GTR !LATEST_VERSION! (
+        set "LATEST_VERSION=!CURRENT_VERSION!"
+    )
+)
+
+for /f "tokens=2*" %%A in ('reg query "%BDS_REG_KEY%\%LATEST_VERSION%" /v RootDir 2^>nul') do (
+    set "DELPHI_PATH=%%B"
+)
+
+if defined DELPHI_PATH (
+    set "DELPHI_BIN_DIRECTORY=%DELPHI_PATH%bin"
+) else (
+    echo Delphi installation not found in Registry.
+    exit /b 1
+)
+
 call clean.bat
-call "%ProgramFiles(x86)%\Embarcadero\Studio\37.0\bin\rsvars.bat"
+call "%DELPHI_BIN_DIRECTORY%\rsvars.bat"
+
+REM Define Required Libraries
+set "BASE_DIR=%~dp0"
 
 REM Format: "ProjectFolder | ProjectFile | OutputFolder"
 for %%A in (
@@ -28,15 +54,15 @@ for %%A in (
                 mkdir "Dist\%%Y\%%K\OpenSSL\" 2>nul
 
                 REM Build Optix NoSSL & Optix OpenSSL
-                msbuild "%%I\%%J.dproj" /t:Build /p:Config=Release /p:Platform=%%X
-                msbuild "%%I\%%J_OpenSSL.dproj" /t:Build /p:Config=Release /p:Platform=%%X
+                msbuild "%%I\%%J.dproj" /t:Build /p:Config=Release /p:Platform=%%X"
+                msbuild "%%I\%%J_OpenSSL.dproj" /t:Build /p:Config=Release /p:Platform=%%X"
 
                 REM Copy Optix NoSSL
                 copy /Y "%%I\bins\NoSSL\%%X\Release\*.exe" "Dist\%%Y\%%K\NoSSL\"
 
                 REM Copy Optix OpenSSL
                 copy /Y "%%I\bins\OpenSSL\%%X\Release\*.exe" "Dist\%%Y\%%K\OpenSSL\"
-                copy /Y "ExtLibraries\LibOpenSSL\%%X\*.dll" "Dist\%%Y\%%K\OpenSSL\"
+                copy /Y "Libraries\LibOpenSSL\%%X\*.dll" "Dist\%%Y\%%K\OpenSSL\"
             )
         )
     )
